7187624be16a4f2c2e5049bbd1ad8d2229b7afad
ContentPI <carlos.santana@dev.education>
2020-11-02 00:12:52 -0800

    Moving i18nfields

--- frontend/src/app/dashboard/components/Content/index.tsx
+++ frontend/src/app/dashboard/components/Content/index.tsx
@@ -19,6 +19,9 @@ import DeleteEntriesModal from '@modals/DeleteEntriesModal'
 import PublishOrUnpublishEntriesModal from '@modals/PublishOrUnpublishEntriesModal'
 import PageNotFound from '../PageNotFound'
 
+// Data
+import i18nFields from './i18nFields'
+
 // Styles
 import { StyledContent } from './Content.styled'
 
@@ -52,71 +55,7 @@ const Content: FC<iProps> = ({ data, router }): ReactElement => {
   if (getModel) {
     fields = getModel.fields
   } else {
-    fields = [
-      {
-        id: '7a99ff36-6b39-4cd6-a4a4-83b6660fe881',
-        type: 'ID',
-        fieldName: 'ID',
-        identifier: 'id',
-        isHide: false,
-        isMedia: false,
-        isPrimaryKey: true,
-        isRequired: true,
-        isSystem: true,
-        isUnique: true,
-        order: '-1',
-        values: []
-      },
-      {
-        id: '1dfb72c4-f863-4932-b5e7-f25480574a35',
-        defaultValue: '',
-        description: '',
-        fieldName: 'Key',
-        identifier: 'key',
-        isHide: false,
-        isMedia: false,
-        isPrimaryKey: false,
-        isRequired: true,
-        isSystem: false,
-        isUnique: false,
-        order: '1',
-        type: 'String',
-        values: []
-      },
-      {
-        id: '1dfb72c4-f863-4932-b5e7-f25480574a36',
-        defaultValue: '',
-        description: '',
-        fieldName: 'Value',
-        identifier: 'value',
-        isHide: false,
-        isMedia: false,
-        isPrimaryKey: false,
-        isRequired: true,
-        isSystem: false,
-        isUnique: false,
-        order: '2',
-        type: 'String',
-        values: []
-      },
-      {
-        id: '1dfb72c4-f863-4932-b5e7-f25480574a37',
-        defaultValue: '',
-        description: '',
-        fieldName: 'Language',
-        identifier: 'language',
-        isHide: false,
-        isMedia: false,
-        isPrimaryKey: false,
-        isRequired: true,
-        isSystem: false,
-        isUnique: false,
-        order: '3',
-        type: 'String',
-        values: []
-      }
-    ]
-
+    fields = i18nFields
     values = getI18n
   }
 
